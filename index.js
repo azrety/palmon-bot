502ab77a04892758f52a55dea76a76d0323a5405
azrety <azrety03@gmail.com>
2026-04-12 16:57:39 +0200

    reset commands

--- index.js
+++ index.js
@@ -37,9 +37,9 @@ async function registerCommands() {
   try {
     console.log("🚀 Enregistrement des slash commands...");
     await rest.put(
-  Routes.applicationGuildCommands(CLIENT_ID, "1491506781245931563"),
-  { body: [] }
-);
+      Routes.applicationGuildCommands(CLIENT_ID, "1491506781245931563"),
+      { body: commands }
+    );
     console.log("✅ Slash command prête !");
   } catch (err) {
     console.error("❌ Erreur register:", err);
