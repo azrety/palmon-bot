794600a1f31efd18786699b5266a6367caadf61f
azrety <azrety03@gmail.com>
2026-05-01 21:10:53 +0200

    deploy bot

--- index.js
+++ index.js
@@ -474,11 +474,6 @@ if (cmd === "searchpalmon") {
     return interaction.editReply("❌ Aucun résultat");
   }
 
-  const lines = results.map(p => {
-    const mons = p.mons.map(m => `• 🟡 ${m}`).join("\n");
-
-    return `👤 **${p.pseudo}**\n${mons}`;
-  });
     const lines = results.map(p => {
       const mons = p.mons.map(formatMon).join("\n");
 
