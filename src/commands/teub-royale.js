ab4efd063e4ef42359199c306ef0e1b74926b158
azrety <azrety03@gmail.com>
2026-07-24 20:14:31 +0200

    Ajout commande teub-royale

--- src/commands/teub-royale.js
+++ src/commands/teub-royale.js
@@ -2,23 +2,6 @@ module.exports = {
   name: "teub-royale",
 
   async execute(interaction) {
-    const mode = interaction.options.getString("mode");
-    const delai = interaction.options.getInteger("delai") ?? 5;
-    const joueurs = interaction.options.getString("joueurs");
-    const exclure = interaction.options.getString("exclure");
-
-    await interaction.reply(
-      `🍆 **TEUB ROYALE**
-
-🇫🇷 La commande fonctionne !
-
-🇬🇧 The command is working!
-
----
-Mode : ${mode}
-Délai : ${delai}s
-Joueurs : ${joueurs ?? "Tous"}
-Exclus : ${exclure ?? "Aucun"}`
-    );
+    await interaction.reply("🍆 Teub Royale fonctionne !");
   }
 };
